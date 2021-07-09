e095e6ce541c9b7bac171b21f4e540b31f6b40ea
dekapaan <terblancheronald@gmail.com>
2021-07-09 15:44:43 +0200

    updated

--- admin_page.py
+++ admin_page.py
@@ -2,6 +2,7 @@ import tkinter
 from tkinter import *
 import mysql.connector
 from tkinter import ttk
+from tkinter import messagebox
 root = Tk()
 root.title("Database Displayed")
 
@@ -20,7 +21,7 @@ s.configure("Treeview.Heading",  background="#187cbd", foreground="black",  font
 
 connect = mysql.connector.connect(host="localhost", user="lifechoices", password="@Lifechoices1234", database="lifechoicesonline")
 conn = connect.cursor()
-conn.execute("SELECT * FROM User")
+conn.execute("SELECT * FROM User ORDER BY No")
 tree = ttk.Treeview(root)
 
 heading_LBL = Label(root, text="Table Of User Details ", bg="white", font="Times 20 bold")
@@ -54,14 +55,15 @@ tree.place(x=20, y=80)
 connect = mysql.connector.connect(host="localhost", user="lifechoices", password="@Lifechoices1234", database="lifechoicesonline")
 conn = connect.cursor()
 conn.execute("SELECT * FROM nextofkin")
-tree = ttk.Treeview(root)
+tree2 = ttk.Treeview(root)
 
 
 # INSERT BUTTON AND FUNCTION
 # HERE A FUNCTION WILL BE CREATED SO THE DATA CAN BE ADDED/INSERTED INTO THE DATABASE
+
 name = tkinter.StringVar()
 email = tkinter.StringVar()
-phone = tkinter.IntVar()
+phone = tkinter.StringVar()
 id = tkinter.StringVar()
 
 def add_data(tree):
@@ -93,8 +95,25 @@ def add_data(tree):
         em = email.get()
         ph = phone.get()
         iden = id.get()
-        conn.execute("INSERT INTO User(Name, Email, Phone, ID) VALUES(%s, %s, %s, %s)", (s_name, em, ph, iden))
+        conn.execute("SELECT * FROM User")
+        current = 0
+        for i in conn:
+            current = int(i[0]) + 1
+        conn.execute("INSERT INTO User(No, Name, Email, Phone, ID) VALUES(%s, %s, %s, %s, %s)", (current, s_name, em, ph, iden))
         print(conn.lastrowid)
+        current = 0
+        for i in conn:
+           print(i)
+        connect.commit()
+        tree.insert('', 'end', text="", values=(conn.lastrowid, current, s_name, em, ph, iden))
+        messagebox.showinfo(message="User added successfully")
+        e1.delete(0,END)
+        e2.delete(0, END)
+        e3.delete(0, END)
+        e4.delete(0, END)
+        f.destroy()
+
+
 
     submit = Button(f, text="Submit", commmand=None, bg="#187bcd", fg="black", font="Times 15 bold", command=insert_data)
     submit.place(x=100, y=280)
@@ -107,30 +126,41 @@ insert_Btn.place(x=240, y=350)
 
 
 
+def delete_user(tree):
+    item_selected=tree.selection()[0]
+    print(tree.item(item_selected)['values'])
+    uid =tree.item(item_selected)['values'][0]
+    del_query="DELETE FROM User WHERE No=%s"
+    sel_data =(uid,)
+    conn.execute(del_query, sel_data)
+    connect.commit()
+    tree.delete(item_selected)
+    messagebox.showinfo("Success", "User data deleted")
 
-delete_Btn = Button(root, text="Sign Out", borderwidth="1", bg="#187bcd", fg="black", font="calibri 15 bold", width=15,
-                        activebackground="green", command=None)
+
+delete_Btn = Button(root, text="Delete", borderwidth="1", bg="#187bcd", fg="black", font="calibri 15 bold", width=15,
+                        activebackground="green", command=lambda: delete_user(tree))
 delete_Btn.place(x=580, y=350)
 
 nextHeading_LBL = Label(root, text="Table Of Next Of Kin Details ", bg="white", font="Times 20 bold")
 nextHeading_LBL.place(x=380, y=440)
-tree["show"] = "headings"
-tree["columns"] = ("No", "Name", "Phone", "UserNo")
-tree.column("No", width=100, minwidth=100, anchor=CENTER)
-tree.column("Name", width=200, minwidth=200, anchor=CENTER)
-tree.column("Phone", width=200, minwidth=200, anchor=CENTER)
-tree.column("UserNo", width=150, minwidth=150, anchor=CENTER)
+tree2["show"] = "headings"
+tree2["columns"] = ("No", "Name", "Phone", "UserNo")
+tree2.column("No", width=100, minwidth=100, anchor=CENTER)
+tree2.column("Name", width=200, minwidth=200, anchor=CENTER)
+tree2.column("Phone", width=200, minwidth=200, anchor=CENTER)
+tree2.column("UserNo", width=150, minwidth=150, anchor=CENTER)
 
-tree.heading("No", text="No", anchor=CENTER)
-tree.heading("Name", text="Name", anchor=CENTER)
-tree.heading("Phone", text="Phone", anchor=CENTER)
-tree.heading("UserNo", text="UserNo", anchor=CENTER)
+tree2.heading("No", text="No", anchor=CENTER)
+tree2.heading("Name", text="Name", anchor=CENTER)
+tree2.heading("Phone", text="Phone", anchor=CENTER)
+tree2.heading("UserNo", text="UserNo", anchor=CENTER)
 
 
 i = 0
 for ro in conn:
-    tree.insert('', i, text="", values=(ro[0], ro[1], ro[2], ro[3]))
+    tree2.insert('', i, text="", values=(ro[0], ro[1], ro[2], ro[3]))
     i = i + 1
-tree.place(x=250, y=500)
+tree2.place(x=250, y=500)
 
 root.mainloop()
